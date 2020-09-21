664ca979dd149e600b2d066265a13d1d47bb359a
EugeneManushkin <eugene.manushkin@gmail.com>
2020-09-21 21:15:57 +0300

    Implemented CtagsSourceNavigator integration script

--- appveyor/build.bat
+++ appveyor/build.bat
@@ -3,6 +3,14 @@ md %BUILD_ROOT%
 cd %BUILD_ROOT% || exit /b 1
 cmake -G "%CMAKE_GENERATOR%" -DYALTA_VERSION_MAJOR=%YALTA_VERSION_MAJOR% -DYALTA_VERSION_MINOR=%YALTA_VERSION_MINOR% -DYALTA_BUILD=%APPVEYOR_BUILD_NUMBER% "%APPVEYOR_BUILD_FOLDER%" || exit /b 1
 cmake --build . --config %CONFIGURATION% || exit /b 1
+
+appveyor DownloadFile https://github.com/universal-ctags/ctags-win32/releases/download/%CTAGS_UTIL_DATE%/%CTAGS_UTIL_REV%/ctags-%CTAGS_UTIL_DATE%_%CTAGS_UTIL_REV%-%ARCH%.zip -FileName ctags.utility.zip || exit /b 1
+set TEMP_DIR=%BUILD_ROOT%\temp
+md %TEMP_DIR% || exit /b 1
+7z x %BUILD_ROOT%\ctags.utility.zip -o%TEMP_DIR% || exit /b 1
+move %TEMP_DIR%\ctags.exe %CONFIGURATION%\%PRODUCT_DIR%\bin || exit /b 1
+move %TEMP_DIR%\license %CONFIGURATION%\%PRODUCT_DIR%\bin || exit /b 1
+
 cd %CONFIGURATION% || exit /b 1
 7z a yalta-%APPVEYOR_BUILD_VERSION%-%ARCH%.zip %PRODUCT_DIR% || exit /b 1
 move *.zip "%APPVEYOR_BUILD_FOLDER%" || exit /b 1
---
+++ scripts/ctags_wrapper.bat
@@ -0,0 +1,28 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+set workdir=%CD%\
+set YALTA=%~dp0bin\yalta.exe
+%~dp0bin\ctags.exe --exclude=*.lua %* || exit /b 1
+call :get_target %*
+if "%TARGET%"=="." set TARGET=*
+if "%TARGET%"=="*" (
+    for /R %%G in (*.lua) do call :index_relative "%%~G" >> tags
+) else call :index_lua "%TARGET%" >> tags
+
+exit /b %errorlevel%
+
+:get_target
+    set "TARGET=%~1"
+    shift
+    if not "%~1"=="" goto get_target
+exit /b
+
+:index_relative
+    set fname=%~dpnx1
+    %YALTA% "!fname:%workdir%=!"
+exit /b
+
+:index_lua
+    if x%~x1==x.lua %YALTA% "%~1"
+    if x%~x1==x.LUA %YALTA% "%~1"
+exit /b
